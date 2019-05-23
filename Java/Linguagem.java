219261fe6c5b7e74c91b9d23ec23312ad220952b
Rayane1007 <rayane.gomes.1007@gmail.com>
2019-05-23 10:12:50 -0300

    Convertido_em_Java

---
+++ Java/Linguagem.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main (String[] args) {
+		for(int x = 50; x >= 20; x--) {
+			if(x % 2 == 0) {
+				System.out.println(x + " é par.");
+			} else {
+				System.out.println(x + " é impar.");
+			}
+		}
+	}
+}
