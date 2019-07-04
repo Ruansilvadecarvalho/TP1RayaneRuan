fd9e8e837b2af045f9886ab2fde620f9849849ff
Ruan <Ruansilvadecarvalhoo@gmail.com>
2019-07-04 09:56:45 -0300

    Banco_de_Dados_Terabyte

--- Gato/src/Terabyte/Cliente.java
+++ Gato/src/Terabyte/Cliente.java
@@ -2,14 +2,154 @@
 package Terabyte;
 
 public class Cliente {
-    String Nome_completo;
-    String Data_de_nascimento;
-    int CPF;
-    int RG;
-    int Telefone_residencial;
-    int Telefone_celular;
-    String Email;
-    String Confirme_email;
-    int Senha;
-    int Confirme_senha;  
+    private String Nome_completo;
+    private String Data_de_nascimento;
+    private String CPF;
+    private String RG;
+    private int Telefone_residencial;
+    private int Telefone_celular;
+    private String Email;
+    private String Confirme_email;
+    private int Senha;
+    private int Confirme_senha;   
+
+    /**
+     * @return the Nome_completo
+     */
+    public String getNome_completo() {
+        return Nome_completo;
+    }
+
+    /**
+     * @param Nome_completo the Nome_completo to set
+     */
+    public void setNome_completo(String Nome_completo) {
+        this.Nome_completo = Nome_completo;
+    }
+
+    /**
+     * @return the Data_de_nascimento
+     */
+    public String getData_de_nascimento() {
+        return Data_de_nascimento;
+    }
+
+    /**
+     * @param Data_de_nascimento the Data_de_nascimento to set
+     */
+    public void setData_de_nascimento(String Data_de_nascimento) {
+        this.Data_de_nascimento = Data_de_nascimento;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the RG
+     */
+    public String getRG() {
+        return RG;
+    }
+
+    /**
+     * @param RG the RG to set
+     */
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    /**
+     * @return the Telefone_residencial
+     */
+    public int getTelefone_residencial() {
+        return Telefone_residencial;
+    }
+
+    /**
+     * @param Telefone_residencial the Telefone_residencial to set
+     */
+    public void setTelefone_residencial(int Telefone_residencial) {
+        this.Telefone_residencial = Telefone_residencial;
+    }
+
+    /**
+     * @return the Telefone_celular
+     */
+    public int getTelefone_celular() {
+        return Telefone_celular;
+    }
+
+    /**
+     * @param Telefone_celular the Telefone_celular to set
+     */
+    public void setTelefone_celular(int Telefone_celular) {
+        this.Telefone_celular = Telefone_celular;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return the Confirme_email
+     */
+    public String getConfirme_email() {
+        return Confirme_email;
+    }
+
+    /**
+     * @param Confirme_email the Confirme_email to set
+     */
+    public void setConfirme_email(String Confirme_email) {
+        this.Confirme_email = Confirme_email;
+    }
+
+    /**
+     * @return the Senha
+     */
+    public int getSenha() {
+        return Senha;
+    }
+
+    /**
+     * @param Senha the Senha to set
+     */
+    public void setSenha(int Senha) {
+        this.Senha = Senha;
+    }
+
+    /**
+     * @return the Confirme_senha
+     */
+    public int getConfirme_senha() {
+        return Confirme_senha;
+    }
+
+    /**
+     * @param Confirme_senha the Confirme_senha to set
+     */
+    public void setConfirme_senha(int Confirme_senha) {
+        this.Confirme_senha = Confirme_senha;
+    }
 }
--- Gato/src/Terabyte/Endereco.java
+++ Gato/src/Terabyte/Endereco.java
@@ -1,14 +1,140 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Terabyte;
 
-/**
- *
- * @author aluno
- */
 public class Endereco {
-    
+   private String Endereco;
+   private String Complemento;
+   private int Numero_da_rua;
+   private String CEP;
+   private String Rua;
+   private String Bairro;
+   private String Pais;
+   private String Cidade;
+   private String Estado;
+
+    /**
+     * @return the Endereco
+     */
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    /**
+     * @param Endereco the Endereco to set
+     */
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    /**
+     * @return the Complemento
+     */
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    /**
+     * @param Complemento the Complemento to set
+     */
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    /**
+     * @return the Numero_da_rua
+     */
+    public int getNumero_da_rua() {
+        return Numero_da_rua;
+    }
+
+    /**
+     * @param Numero_da_rua the Numero_da_rua to set
+     */
+    public void setNumero_da_rua(int Numero_da_rua) {
+        this.Numero_da_rua = Numero_da_rua;
+    }
+
+    /**
+     * @return the CEP
+     */
+    public String getCEP() {
+        return CEP;
+    }
+
+    /**
+     * @param CEP the CEP to set
+     */
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    /**
+     * @return the Rua
+     */
+    public String getRua() {
+        return Rua;
+    }
+
+    /**
+     * @param Rua the Rua to set
+     */
+    public void setRua(String Rua) {
+        this.Rua = Rua;
+    }
+
+    /**
+     * @return the Bairro
+     */
+    public String getBairro() {
+        return Bairro;
+    }
+
+    /**
+     * @param Bairro the Bairro to set
+     */
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    /**
+     * @return the Pais
+     */
+    public String getPais() {
+        return Pais;
+    }
+
+    /**
+     * @param Pais the Pais to set
+     */
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+
+    /**
+     * @return the Cidade
+     */
+    public String getCidade() {
+        return Cidade;
+    }
+
+    /**
+     * @param Cidade the Cidade to set
+     */
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    /**
+     * @return the Estado
+     */
+    public String getEstado() {
+        return Estado;
+    }
+
+    /**
+     * @param Estado the Estado to set
+     */
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
 }
