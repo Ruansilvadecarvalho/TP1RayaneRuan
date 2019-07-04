9e8c19ec7bf5104996128fc5ef43373eda5f0222
Rayane1007 <rayane.gomes.1007@gmail.com>
2019-07-04 09:07:02 -0300

    Projeto_Banco_de_Dados

---
+++ Gato/src/Terabyte/Cliente.java
@@ -0,0 +1,15 @@
+
+package Terabyte;
+
+public class Cliente {
+    String Nome_completo;
+    String Data_de_nascimento;
+    int CPF;
+    int RG;
+    int Telefone_residencial;
+    int Telefone_celular;
+    String Email;
+    String Confirme_email;
+    int Senha;
+    int Confirme_senha;  
+}
